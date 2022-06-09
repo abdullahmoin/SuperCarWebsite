0dc2e930bf01f1db6e890950bd2541a8ee228004
Dbg-Moin <Dbg-Moin@users.github.com>
2022-06-10 04:34:17 +0600

    popular section

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,30 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== POPULAR SWIPER ===============*/
 
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor:true,
+
+
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+
+    breakpoints: {
+        
+        768: {
+          slidesPerView: 3,
+
+        },
+        1024: {
+          
+          spaceBetween: 48,
+        },
+      },
+});
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
