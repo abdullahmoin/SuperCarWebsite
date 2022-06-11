e40a59086d65f792c6c75556fd05c091aef32bc1
Dbg-Moin <Dbg-Moin@users.github.com>
2022-06-11 20:40:02 +0600

    completed mainframe

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,14 +67,76 @@ let swiperPopular = new Swiper(".popular__container", {
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
+let mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active featured */
 
+const linkFeatured = document.querySelectorAll('.featured__item');
+
+function activeFeatured(){
+    linkFeatured.forEach(l=>l.classList.remove('active-featured'));
+    this.classList.add('active-featured');
+}
+
+linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured));
+
 
 /*=============== SHOW SCROLL UP ===============*/
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+})
+
+sr.reveal(`.home__title, .popular__container, .features__img, .featured__filters`);
+sr.reveal(`.home__subtitle`, {delay: 500});
+sr.reveal(`.home__elec`, {delay: 600});
+sr.reveal(`.home__img`, {delay: 800});
+sr.reveal(`.home__car-data, .footer__copy`, {delay: 900, interval: 100, origin: 'bottom'});
+sr.reveal(`.home__button`, {delay: 1000, origin: 'bottom'});
+
+sr.reveal(`.about__group, .offer__data`, {origin: 'left'});
+sr.reveal(`.about__data, .offer__img`, {origin: 'right'});
+
+sr.reveal(`.features__map`, {delay:600, origin: 'bottom'});
+sr.reveal(`.features__card`, {interval: 300});
+sr.reveal(`.featured__card, .logos__content, .footer__content`, {interval: 100});
